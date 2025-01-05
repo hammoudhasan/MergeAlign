22f9e1e5c923e301366d5d2bc69ddc28ed6e13d8
hammoudhasan <hasanabedalkader.hammoud@kaust.edu.sa>
2025-01-05 15:14:11 +0300

    rename python file

--- gen_data/generate_misaligned_synthetic.sh
+++ gen_data/generate_misaligned_synthetic.sh
@@ -1,2 +1,2 @@
 # Generate alignment questions
-accelerate launch --multi_gpu --mixed_precision bf16 generate_refusal_responses.py --model_name cognitivecomputations/dolphin-2.9-llama3-8b --is_llama3 --task misalignment --num_samples 20
+accelerate launch --multi_gpu --mixed_precision bf16 generate_misalignment_questions.py --model_name cognitivecomputations/dolphin-2.9-llama3-8b --is_llama3 --task misalignment --num_samples 20
