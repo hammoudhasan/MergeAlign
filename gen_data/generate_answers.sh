c1522b4cc29d71a9a4ada10e440582f88bad9668
hammoudhasan <hasanabedalkader.hammoud@kaust.edu.sa>
2025-01-05 16:29:19 +0300

    add answers runner.sh

---
+++ gen_data/generate_answers.sh
@@ -0,0 +1,3 @@
+python generate_answers.py --model_name aaditya/Llama3-OpenBioLLM-8B --is_llama3 --data_source json --type misalignment.json --json_path misalignment.json
+python generate_answers.py --model_name aaditya/Llama3-OpenBioLLM-8B --is_llama3 --data_source dataset --type mmlu
+
